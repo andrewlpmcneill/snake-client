70d637dc8eb4f7f9b3727e29d2c7a72c557d0a6a
Andrew McNeill <andrew@andrewmc.ca>
2022-01-19 16:14:33 -0400

    Moved all input functions to a separate input file

---
+++ input.js
@@ -0,0 +1,20 @@
+// setup interface to handle user input from stdin
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    console.log('You have quit!');
+    process.exit();
+  }
+};
+
+module.exports = {
+  setupInput,
+};
