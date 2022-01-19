2c40f03cc69f5dc39fd7a0bfb6a36e3ad1602608
Andrew McNeill <andrew@andrewmc.ca>
2022-01-19 16:29:14 -0400

    Final game functionality

--- input.js
+++ input.js
@@ -1,9 +1,13 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = () => {
+const setupInput = (conn) => {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  connection = conn;
   stdin.on("data", handleUserInput);
   return stdin;
 };
@@ -12,6 +16,21 @@ const handleUserInput = (key) => {
   if (key === '\u0003') {
     console.log('You have quit!');
     process.exit();
+  }
+
+  switch (key) {
+  case 'w':
+    connection.write('Move: up');
+    break;
+  case 'a':
+    connection.write('Move: left');
+    break;
+  case 's':
+    connection.write('Move: down');
+    break;
+  case 'd':
+    connection.write('Move: right');
+    break;
   }
 };
 
