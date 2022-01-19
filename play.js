7be719a91e56314df2fd2afa75f32ab2d36d47bd
Andrew McNeill <andrew@andrewmc.ca>
2022-01-19 16:11:22 -0400

    Some stdin setup

--- play.js
+++ play.js
@@ -4,3 +4,23 @@ const { connect } = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    console.log('You have quit!');
+    process.exit();
+  }
+};
+
+setupInput();
